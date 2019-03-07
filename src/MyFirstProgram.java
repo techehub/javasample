eca91013aa615002fa1b01fae3861df9d71b4cae
techehub <vijeesh.tp@techehub.com>
2019-03-07 15:47:06 +0530

    Added sum logic

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -1,7 +1,11 @@
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a=10;
+        int b=20;
+	int sum= a+b;
+	System.out.println("Hello");
+   	
     }
 
 }
